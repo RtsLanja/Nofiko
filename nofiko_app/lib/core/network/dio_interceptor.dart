bc23fd5072e0c253af450aa3f66263d80880ccf2
RtsLanja <rantorazaka12@gmail.com>
2026-03-20 08:56:38 +0300

    ammelioration en prod

--- nofiko_app/lib/core/network/dio_interceptor.dart
+++ nofiko_app/lib/core/network/dio_interceptor.dart
@@ -17,17 +17,27 @@ void setupInterceptor(Dio dio, AuthService authService) {
       },
       onError: (error, handler) async {
         final options = error.requestOptions;
+
         if (error.response?.statusCode == 401 &&
             options.extra["retried"] != true) {
           options.extra["retried"] = true;
 
           final newToken = await authService.refreshToken();
+
           if (newToken != null) {
             options.headers["Authorization"] = "Bearer $newToken";
-            final cloneReq = await dio.fetch(options);
-            return handler.resolve(cloneReq);
+
+            try {
+              final cloneReq = await dio.fetch(options);
+              return handler.resolve(cloneReq);
+            } catch (e) {
+              return handler.next(error);
+            }
           }
+          await authService.logout();
+          return handler.next(error);
         }
+
         return handler.next(error);
       },
     ),
